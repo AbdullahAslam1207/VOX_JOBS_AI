40d7166a685d2209da7837ef9d866582c16d6946
Abdullah <am0055461@gmail.com>
2025-10-03 02:09:38 +0500

    Jobz Scraper Implemented

---
+++ Security/security.py
@@ -0,0 +1,20 @@
+import os
+from fastapi import FastAPI, Depends, HTTPException, Security
+from fastapi.security.api_key import APIKeyHeader
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_KEY = os.getenv("API_KEY")
+API_KEY_NAME = os.getenv("API_KEY_NAME")  
+
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+
+async def verify_api_key(api_key: str = Security(api_key_header)):
+    if api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    return api_key
+
+
+
--- application.py
+++ application.py
@@ -1,5 +1,16 @@
-from fastapi import FastAPI
+import httpx
+import json
+import asyncio
+from pathlib import Path
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from Routes.fetch_mustaqbil_jobs import fetch_jobs
+from Routes.fetch_jobz_jobs import fetch_jobz_jobs
+from Security.security import verify_api_key
+import os
+
+load_dotenv()
 
 application = FastAPI()
 
@@ -15,3 +26,18 @@ application.add_middleware(
 @application.get("/")
 def home():
     return {"message": "Welcome to VoxJobs AI home route!"}
+
+
+
+
+@application.get("/fetch_jobs_mustaqbil")
+async def fetch_jobs_mustaqbil(api_key: str = Depends(verify_api_key)):
+    result = await fetch_jobs()
+    return result
+
+
+@application.get("/fetch_jobs_jobz")
+async def fetch_jobs_jobz(api_key: str = Depends(verify_api_key)):
+    result = await fetch_jobz_jobs()
+    return result
+    
